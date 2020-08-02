0d208465e482b7d86634b3c2096306a403729e91
tangkaiqiang <justforgit@163.com>
2020-08-02 21:49:32 +0800

    解决scroll滚动条挤压内容宽度的问题

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -3,9 +3,16 @@ import './index.scss';
 import logo from '../../assets/images/logo.png';
 
 const TopBar = () => (
-  <div className="top-bar">
-    <img src={logo} alt="logo" className="logo" />
-    <span className="logo-text">A Drop</span>
+  <div className="top-bar-container">
+    <div className="top-bar">
+      <img src={logo} alt="logo" className="logo" />
+      <span className="logo-text">A Drop</span>
+      <div className="top-nav">
+        <div className="top-nav-item">
+          <a href="https://github.com/justforfunmy">更多开源项目</a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
